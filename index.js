1b9677a085175a887663291a77c467237441e593
JustWhaley <67840167+JustWhaley@users.noreply.github.com>
2020-07-27 15:29:39 +0200

    init

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ client.on('ready', async () => {
     console.log(`Im vibin with my music`);
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+        client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
     }, 10000);
 });
 
