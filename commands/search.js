0f647945f1bcbc3dc0d2a37a82e5d5b9558f4613
JustWhaley <67840167+JustWhaley@users.noreply.github.com>
2020-08-03 16:59:52 +0200

    init

---
+++ commands/search.js
@@ -0,0 +1,50 @@
+const discord = require("discord.js");
+const search = require("yt-search");
+module.exports.run = async (client, message, args, ops) => {
+
+    search(args.join(" "), function (err, res){
+
+        if(err) return message.channel.send("Something went wrong");
+
+        var vids = res.videos.slice(0,10);
+
+        var response = "";
+
+        for(var vid in videos){
+            response += `**[${parseInt(vid) + 1}]:** ${vids[vid].title} \r\n`;
+        }
+
+        response += `Please choose a number between 0 and ${vids.length}`
+
+        message.channel.send(response);
+
+
+        const filter = music => !isNaN(music.content) && music.content < vids.length + 1 && music.content > 0;
+
+        const collection = message.channel.createMessageCollector(filter);
+
+        collection.vidas = vids;
+
+        collection.once("collect", function (musica){
+
+            var commandFile = require("./play.js");
+
+            commandFile.run(client, message, [this.vidas[parseInt(musica.content) -1].url], ops);
+
+        });
+
+
+
+
+    });
+
+
+    
+    
+
+
+}
+
+module.exports.help = {
+    name: "search"
+}
