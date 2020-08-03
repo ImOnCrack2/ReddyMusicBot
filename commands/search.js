d16c41500ec2e84040fa4bcc4d120e6f744361b3
JustWhaley <67840167+JustWhaley@users.noreply.github.com>
2020-08-03 17:36:59 +0200

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -1,49 +1,47 @@
-const discord = require("discord.js");
 const search = require("yt-search");
+
+
 module.exports.run = async (client, message, args, ops) => {
 
-    search(args.join(" "), function (res){
+    search(args.join(" "), function (err, res){
 
+        if (err) return message.channel.send("Something went wrong, but what? :thinking:");
 
-        var vids = res.vids.slice(0,10)
+        var videos = res.videos.slice(0,10);
 
         var response = "";
 
         for(var vid in videos){
-            response += `**[${parseInt(vid) + 1}]:** ${vids[vid].title} \r\n`;
+            response += `**[${parseInt(vid) +1}]:** ${videos[vid].title} \r\n`;
+            
         }
 
-        response += `Please choose a number between 0 and ${vids.length}`
+        response += `Please choose a number between 0 and ${videos.length}`;
 
         message.channel.send(response);
 
-
-        const filter = music => !isNaN(music.content) && music.content < vids.length + 1 && music.content > 0;
+        const filter = music => !isNaN(music.content) && music.content < videos.length +1 && music.content > 0;
 
         const collection = message.channel.createMessageCollector(filter);
 
-        collection.vidas = vids;
+        collection.videos = videos;
 
-        collection.once("collect", function (musica){
+        collection.once("collect", function (music){
 
             var commandFile = require("./play.js");
 
-            commandFile.run(client, message, [this.vidas[parseInt(musica.content) -1].url], ops);
+            commandFile.run(client, message, [this.videos[parseInt(music.content) -1].url], ops);
 
         });
-
-
 
 
     });
 
 
-    
-    
 
 
 }
 
 module.exports.help = {
-    name: "search"
+    name: "hallo"
 }
